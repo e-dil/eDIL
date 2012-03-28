4751f4fa4dfe5155aeab762e5e55ad6ed9ea651e
Conail Stewart <conail@conail.net>
2012-03-28 14:26:18 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DilIe::Application.config.secret_token = '5194168104be71a5c8f41c29d79c815db9c512a0054bd308e70205822412189e9ffa42cd16539c74fc18f626beebce24289e3b310981b88622b18910b2bf0ded'
